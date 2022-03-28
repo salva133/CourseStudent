9ba0986c183f2e0e2b50f0de7021b15c0fa584a6
salva133 <34437542+salva133@users.noreply.github.com>
2022-03-28 19:12:35 +0200

    removed ProjectException and logs for now implemented hibernate-core

--- src/main/java/com/learning/exceptions/ProjectException.java
+++
@@ -1,7 +0,0 @@
-package com.learning.exceptions;
-
-public class ProjectException extends Exception {
-    public String FailedCreationException() {
-        return "ERROR: Creation of Objects failed....";
-    }
-}
