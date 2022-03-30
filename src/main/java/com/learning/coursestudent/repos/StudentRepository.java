10323d3b845e03c7fba242e221c2a06c238bc4dc
Toni <c.santana@headbeat.de>
2022-03-30 22:10:25 +0200

    removed List<Student> findAll(); from StudentRepository

--- src/main/java/com/learning/coursestudent/repos/StudentRepository.java
+++ src/main/java/com/learning/coursestudent/repos/StudentRepository.java
@@ -9,7 +9,6 @@ import java.util.List;
 @Repository
 public interface StudentRepository extends CrudRepository<Student, Long> {
 
-    List<Student> findAll();
     Student findByStudentId(long studentId);
     List<Student> findByLastName(String lastName);
     List<Student> findByFirstName(String firstName);
