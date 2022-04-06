6851a81159007b6334fb6b7d2b7ec6c0230d448c
salva133 <34437542+salva133@users.noreply.github.com>
2022-04-06 09:14:18 +0200

    renamed ReturnObjectAsJSON.java to Statics.java

--- src/main/java/com/learning/coursestudent/classes/Statics.java
+++ src/main/java/com/learning/coursestudent/classes/Statics.java
@@ -2,7 +2,7 @@ package com.learning.coursestudent.classes;
 
 import com.google.gson.Gson;
 
-public class ReturnObjectAsJSON {
+public class Statics {
     public static String returnObjectAsJSON(Object object) {
         Gson gson = new Gson();
         return gson.toJson(object);
