4d9ed03f9fe836d1d0d02b5d9fa554b46f66f7bc
salva133 <34437542+salva133@users.noreply.github.com>
2022-04-07 18:36:58 +0200

    planned next method for REST

--- src/main/java/com/learning/coursestudent/repos/CourseRepository.java
+++ src/main/java/com/learning/coursestudent/repos/CourseRepository.java
@@ -1,12 +1,17 @@
 package com.learning.coursestudent.repos;
 
 import com.learning.coursestudent.classes.Course;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CourseRepository extends CrudRepository<Course, Long> {
 
-    Course findByCourseId(long courseId);
-    Course findByCourseName(String courseName);
+    @Query("select u from course")
+    List<Course> getAllCourses();
+    Course getByCourseId(long courseId);
+    Course getByCourseName(String courseName);
 }
