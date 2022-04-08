7c6a8d0759e1e04e2bea5d0b28b884aa90d6788a
salva133 <34437542+salva133@users.noreply.github.com>
2022-04-08 14:36:37 +0200

    Repositories now extend JpaRepository

--- src/main/java/com/learning/coursestudent/repos/CourseRepository.java
+++ src/main/java/com/learning/coursestudent/repos/CourseRepository.java
@@ -1,11 +1,11 @@
 package com.learning.coursestudent.repos;
 
 import com.learning.coursestudent.classes.Course;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CourseRepository extends CrudRepository<Course, Long> {
+public interface CourseRepository extends JpaRepository<Course, Long> {
 
     Course getByCourseId(long courseId);
     Course getByCourseName(String courseName);
--- src/main/java/com/learning/coursestudent/repos/StudentRepository.java
+++ src/main/java/com/learning/coursestudent/repos/StudentRepository.java
@@ -1,14 +1,13 @@
 package com.learning.coursestudent.repos;
 
 import com.learning.coursestudent.classes.Student;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface StudentRepository extends CrudRepository<Student, Long> {
+public interface StudentRepository extends JpaRepository<Student, Long> {
 
     Student getByStudentId(long studentId);
     Student getByLastName(String lastName);
