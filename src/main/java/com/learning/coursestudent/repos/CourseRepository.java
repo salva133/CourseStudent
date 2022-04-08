7bbd106b712a20b4f522f96d1e7571b012e6e7e1
salva133 <34437542+salva133@users.noreply.github.com>
2022-04-08 14:28:53 +0200

    removed unused lines

--- src/main/java/com/learning/coursestudent/repos/CourseRepository.java
+++ src/main/java/com/learning/coursestudent/repos/CourseRepository.java
@@ -1,11 +1,8 @@
 package com.learning.coursestudent.repos;
 
 import com.learning.coursestudent.classes.Course;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface CourseRepository extends CrudRepository<Course, Long> {
