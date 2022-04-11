ac93f0730d433e7459d4f2b8ea5a1bebc83f3050
salva133 <34437542+salva133@users.noreply.github.com>
2022-04-11 15:02:01 +0200

    reorganised classes and added class CustomException.java

---
+++ src/main/java/com/learning/coursestudent/classes/CustomException.java
@@ -0,0 +1,7 @@
+package com.learning.coursestudent.classes;
+
+public class CustomException extends Exception {
+    public void StudentTooYoungException(String studentFullName, long studentId) {
+
+    }
+}
--- src/main/java/com/learning/coursestudent/classes/associations/CourseStudent.java
+++
@@ -1,4 +0,0 @@
-package com.learning.coursestudent.classes.associations;
-
-public class CourseStudent {
-}
