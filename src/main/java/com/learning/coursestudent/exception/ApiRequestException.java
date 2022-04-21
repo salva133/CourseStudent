8fc935e03bbd88e2c8345a6a1dd735e2dde58c4b
salva133 <c.santana@headbeat.de>
2022-04-21 16:08:52 +0200

    completely reworked CustomException.java by renaming it, emptying it and giving it parent constructors the "exceptions" got repurposed to "validations" and moved to Student class

---
+++ src/main/java/com/learning/coursestudent/exception/ApiRequestException.java
@@ -0,0 +1,12 @@
+package com.learning.coursestudent.exception;
+
+public class ApiRequestException extends RuntimeException {
+
+    public ApiRequestException(String message) {
+        super(message);
+    }
+
+    public ApiRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
