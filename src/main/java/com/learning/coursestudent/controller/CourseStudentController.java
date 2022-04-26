6dfd7cc69441da98175464cfc6ce79663137599d
salva133 <c.santana@headbeat.de>
2022-04-26 17:46:19 +0200

    removed unnecessary code reworked remaining code

--- src/main/java/com/learning/coursestudent/controller/CourseStudentController.java
+++ src/main/java/com/learning/coursestudent/controller/CourseStudentController.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -47,7 +46,9 @@ public class CourseStudentController {
     //GETTER
 
     //POSTER
-    @PostMapping(value = "set-course")
+
+    // Benötigt Überarbeitung
+    /*@PostMapping(value = "set-course")
     public String setCourseForStudent(StudentPojo studentPojo) {
 
         try {
@@ -58,8 +59,7 @@ public class CourseStudentController {
             System.out.println("A Name for the course was expected");
         }
         return "setCourseForStudent check";
-    }
-
+    }*/
 
     @PostMapping(value = "course")
     public String newCourse(@RequestBody CoursePojo coursePojo) {
@@ -75,23 +75,6 @@ public class CourseStudentController {
         }
         return "newCourse check";
     }
-
-    @PostMapping(value = "course-obj")
-    public Course newCourseObj(@RequestBody CoursePojo coursePojo) {
-        try {
-            Course course = new Course(coursePojo);
-            courseRepository.save(course);
-            System.out.println("## Course \"" + course.getCourseName() + "\" has been created ##");
-            return course;
-        } catch (NameExpectedException e) {
-            System.out.println("A name was expected");
-        } catch (DataIntegrityViolationException e) {
-            System.out.println("Data integrity has been violated, rethrowing to ApiRequestException");
-            throw new ApiRequestException("");
-        }
-        return null;
-    }
-
 
     @PostMapping(value = "student")
     public String newStudent(@RequestBody StudentPojo studentPojo) {
@@ -115,75 +98,12 @@ public class CourseStudentController {
         return "newStudent check";
     }
 
-    @PostMapping(value = "student-obj")
-    public Student newStudentObj(@RequestBody StudentPojo studentPojo) {
-
-        try {
-            Student student = new Student(studentPojo, ageLimit,null);
-            studentRepository.save(student);
-            System.out.println("## Student \"" + student.getFullName() + "\" created ##");
-            return student;
-        } catch (NullPointerException e) {
-            System.out.println("Data of \"" + studentPojo.getLastName() + ", " + studentPojo.getFirstName() + "\" is null");
-        } catch (DateTimeParseException e) {
-            System.out.println("The date of \"" + studentPojo.getLastName() + ", " + studentPojo.getFirstName() + "\" could not be parsed");
-        } catch (DateIsNullException e) {
-            System.out.println("Date of \"" + studentPojo.getLastName() + ", " + studentPojo.getFirstName() + "\" is null");
-        } catch (AgeException e) {
-            System.out.println("The Age of \"" + studentPojo.getLastName() + ", " + studentPojo.getFirstName() + "\" is not valid");
-        } catch (DateFormatException e) {
-            System.out.println("Date Format of \"" + studentPojo.getLastName() + ", " + studentPojo.getFirstName() + "\" is not valid");
-        }
-        return null;
-    }
-
-/*
-    @PostMapping(value = "student-with-course")
-    public String newStudentWithCourse(@RequestBody StudentPojo studentPojo) {
-
-        try {
-            Course course = newCourseObj(coursePojo);
-            Student student = newStudentObj(studentPojo);
-            student.setCourse(course);
-
-*/
-/*            Student student = new Student(studentPojo, ageLimit);
-            studentRepository.save(student);
-            System.out.println("## student saved ##");
-
-            Course course = new Course(coursePojo);
-            courseRepository.save(course);
-            System.out.println("## course saved ##");
-
-            student.setCourse(course);
-            System.out.println("## course set ##");*//*
-
-        } catch (NullPointerException e) {
-            System.out.println("Data of \"" + studentPojo.getLastName() + ", " + studentPojo.getFirstName() + "\" is null");
-        } catch (DateTimeParseException e) {
-            System.out.println("The date of \"" + studentPojo.getLastName() + ", " + studentPojo.getFirstName() + "\" could not be parsed");
-        } catch (DateIsNullException e) {
-            System.out.println("Date of \"" + studentPojo.getLastName() + ", " + studentPojo.getFirstName() + "\" is null");
-        } catch (AgeException e) {
-            System.out.println("The Age of \"" + studentPojo.getLastName() + ", " + studentPojo.getFirstName() + "\" is not valid");
-        } catch (DateFormatException e) {
-            System.out.println("Date Format of \"" + studentPojo.getLastName() + ", " + studentPojo.getFirstName() + "\" is not valid");
-        } catch (NameExpectedException e) {
-            System.out.println("Name of \"" + coursePojo.getCourseName() + "\" is not valid");
-        } catch (HttpMessageNotReadableException e) {
-            System.out.println("Http Message is not readable");
-        }
-        return "newStudentWithCourse check";
-    }
-*/
-
-
     @PostMapping(value = "student-batch")
     public String newStudentBatch(@RequestBody List<StudentPojo> studentPojoList) {
 
         for (StudentPojo pojo : studentPojoList) {
             try {
-                Student student = new Student(pojo, ageLimit,null);
+                Student student = new Student(pojo, ageLimit, null);
                 studentRepository.save(student);
                 System.out.println("##record created##");
             } catch (NullPointerException e) {
