bed819f8cfdaa4a411d92638534e8cd726a542e3
salva133 <c.santana@headbeat.de>
2022-05-06 14:48:40 +0200

    adding test

--- src/test/java/com/learning/coursestudent/CourseStudentApplicationTests.java
+++ src/test/java/com/learning/coursestudent/CourseStudentApplicationTests.java
@@ -1,13 +1,22 @@
 package com.learning.coursestudent;
 
+import com.learning.coursestudent.classes.StudentPojo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.util.Set;
 
 @SpringBootTest
 class CourseStudentApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
+    public void StudentList(Set<StudentPojo> pojo) {
+        Assert.notNull(pojo, "pojo must contain at least one object");
+        Assert.noNullElements(pojo, "pojo object must not contain any null value");
+    }
 }
