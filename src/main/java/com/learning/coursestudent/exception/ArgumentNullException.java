cbe82b34b9882c20815692b391d31eb066cfa323
salva133 <34437542+salva133@users.noreply.github.com>
2022-05-10 16:11:50 +0200

    removed no-args constructor of ArgumentNullException a String has to be provided, or else...

--- src/main/java/com/learning/coursestudent/exception/ArgumentNullException.java
+++ src/main/java/com/learning/coursestudent/exception/ArgumentNullException.java
@@ -1,8 +1,6 @@
 package com.learning.coursestudent.exception;
 
 public class ArgumentNullException extends NullPointerException {
-    public ArgumentNullException() {
-    }
 
     public ArgumentNullException(String s) {
         super(s);
