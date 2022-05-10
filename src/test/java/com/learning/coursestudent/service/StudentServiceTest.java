bf42b223386f376bcbbf2c5a4f5cb1a4cace3317
salva133 <34437542+salva133@users.noreply.github.com>
2022-05-10 14:59:49 +0200

    transfer for fixing, issue is already open

--- src/test/java/com/learning/coursestudent/service/StudentServiceTest.java
+++ src/test/java/com/learning/coursestudent/service/StudentServiceTest.java
@@ -1,8 +1,8 @@
 package com.learning.coursestudent.service;
 
 import com.learning.coursestudent.classes.StudentPojo;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,11 +10,15 @@ class StudentServiceTest {
 
     private StudentService service;
 
-    @Test
-    void testCreateStudent() {
+    @BeforeEach
+    void setUpStudentServiceTest() {
         StudentPojo pojo = new StudentPojo();
         pojo.setLastName("Hansen");
         service.createStudent(pojo);
+    }
+
+    @Test
+    void testCreateStudent() {
         String lastName = String.valueOf(service.studentRepository.getByLastName("Hansen"));
         assertEquals("Hansen", lastName);
     }
