47bbfcff8d9d1ada49469a1cf2c404f4b1b564d3
salva133 <34437542+salva133@users.noreply.github.com>
2022-05-11 15:37:57 +0200

    added JUnit Jupiter API and Mockito Core

--- src/test/java/com/learning/coursestudent/service/StudentServiceTest.java
+++ src/test/java/com/learning/coursestudent/service/StudentServiceTest.java
@@ -3,15 +3,21 @@ package com.learning.coursestudent.service;
 import com.learning.coursestudent.classes.StudentPojo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
-    private final StudentService service;
+    @Mock
+    private StudentService service;
 
     public StudentServiceTest(StudentService service) {
         this.service = service;
