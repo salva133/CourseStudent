d5ff03c6a0fbe874dff669fd381478211b074faa
salva133 <c.santana@headbeat.de>
2022-05-13 14:45:05 +0200

    transfer for tech sync

--- src/test/java/com/learning/coursestudent/service/StudentServiceTest.java
+++ src/test/java/com/learning/coursestudent/service/StudentServiceTest.java
@@ -2,6 +2,7 @@ package com.learning.coursestudent.service;
 
 import com.learning.coursestudent.classes.Course;
 import com.learning.coursestudent.classes.CoursePojo;
+import com.learning.coursestudent.classes.Student;
 import com.learning.coursestudent.classes.StudentPojo;
 import com.learning.coursestudent.exception.DateFormatException;
 import com.learning.coursestudent.exception.DobInFutureException;
@@ -14,6 +15,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -89,5 +92,16 @@ class StudentServiceTest {
         when(courseRepository.findByName(any())).thenReturn(new Course(coursePojo));
 
         assertThrows(TooYoungException.class, () -> service.createStudent(studentPojo));
+    }
+
+    @Test
+    void should_get_record_when_using_findbyId_method() {
+        StudentPojo studentPojo = new StudentPojo();
+        studentPojo.setFirstName("Star");
+        studentPojo.setLastName("Lord");
+        studentPojo.setCourseName("Algebra");
+        studentPojo.setDateOfBirth("2022-05-12");
+
+        when(studentRepository.findById(1L)).thenReturn(Optional.of(new Student()));
     }
 }
