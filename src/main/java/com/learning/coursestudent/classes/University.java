b1f2600678aea5ea56ae56e2409a23014cc87717
salva133 <c.santana@headbeat.de>
2022-05-16 12:25:39 +0200

    added new class

---
+++ src/main/java/com/learning/coursestudent/classes/University.java
@@ -0,0 +1,9 @@
+package com.learning.coursestudent.classes;
+
+public class University {
+    private final String name;
+
+    public University() {
+        this.name = "University of Stuff and Things";
+    }
+}
