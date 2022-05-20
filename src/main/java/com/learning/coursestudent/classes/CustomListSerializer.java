b03be5dd0496f27719f538caa6a88699d65c3479
salva133 <34437542+salva133@users.noreply.github.com>
2022-05-20 16:27:43 +0200

    prepped for Task SBT-23

---
+++ src/main/java/com/learning/coursestudent/classes/CustomListSerializer.java
@@ -0,0 +1,33 @@
+package com.learning.coursestudent.classes;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomListSerializer extends StdSerializer<List<Student>> {
+    public CustomListSerializer() {
+        this(null);
+    }
+    public CustomListSerializer(Class<List<Student>> t) {
+        super(t);
+    }
+
+    @Override
+    public void serialize(
+            List<Student> students,
+            JsonGenerator generator,
+            SerializerProvider provider)
+            throws IOException {
+
+        List<Integer> ids = new ArrayList<>();
+        for (Student student : students) {
+            ids.add((int) student.getId());
+        }
+        generator.writeObject(ids);
+    }
+}
