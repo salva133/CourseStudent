4292988dc7f77ba46a4489e17aea398d2955d15a
salva133 <34437542+salva133@users.noreply.github.com>
2022-05-25 10:48:59 +0200

    emptied CourseStudentApplicationTests because the Test there was of no use

--- src/test/java/com/learning/coursestudent/CourseStudentApplicationTests.java
+++ src/test/java/com/learning/coursestudent/CourseStudentApplicationTests.java
@@ -1,18 +1,7 @@
 package com.learning.coursestudent;
 
-import com.learning.coursestudent.classes.StudentPojo;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
-
-import java.util.Set;
 
 @SpringBootTest
 class CourseStudentApplicationTests {
-
-    @Test
-    public void StudentList(Set<StudentPojo> pojo) {
-        Assert.notNull(pojo, "pojo must contain at least one object");
-        Assert.noNullElements(pojo, "pojo object must not contain any null value");
-    }
 }
