a2aa65c94496dcee0fdc8b26cbcffc629b57c636
salva133 <34437542+salva133@users.noreply.github.com>
2022-06-02 17:34:43 +0200

    added test file

---
+++ src/test/java/com/learning/coursestudent/service/CourseServiceTest.java
@@ -0,0 +1,23 @@
+package com.learning.coursestudent.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CourseServiceTest {
+
+    @Test
+    void getAllCourses() {
+
+    }
+
+    @Test
+    void createCourse() {
+    }
+
+    @Test
+    void createCourseBatch() {
+    }
+}
