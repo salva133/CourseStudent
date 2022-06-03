36c9ac1da7dfd9ed73622593d90e6fc967d8adae
salva133 <34437542+salva133@users.noreply.github.com>
2022-06-03 11:13:02 +0200

    added repo method findCoursesByName

--- src/main/java/com/learning/coursestudent/repository/CourseRepository.java
+++ src/main/java/com/learning/coursestudent/repository/CourseRepository.java
@@ -4,10 +4,14 @@ import com.learning.coursestudent.classes.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
     Course findById(long id);
 
-    Course findByName(String name);
+    Course findCourseByName(String name);
+
+    List<Course> findCoursesByName(String name);
 }
