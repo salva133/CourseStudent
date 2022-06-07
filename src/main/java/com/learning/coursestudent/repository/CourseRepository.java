daffbcb0210077acf94bef18481ca85a0bfc45e3
salva133 <34437542+salva133@users.noreply.github.com>
2022-06-07 15:59:57 +0200

    added findCourseByName

--- src/main/java/com/learning/coursestudent/repository/CourseRepository.java
+++ src/main/java/com/learning/coursestudent/repository/CourseRepository.java
@@ -5,14 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Set;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
     Course findById(long id);
 
-    Set<Course> findCourseByName(String name);
+    Course findCourseByName(String name);
 
     List<Course> findCoursesByName(String name);
 }
