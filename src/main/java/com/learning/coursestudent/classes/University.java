396653cbddf65d6ac19f2d1df7b4d178c5c98261
salva133 <34437542+salva133@users.noreply.github.com>
2022-06-09 11:58:52 +0200

    adapted University class

--- src/main/java/com/learning/coursestudent/classes/University.java
+++ src/main/java/com/learning/coursestudent/classes/University.java
@@ -2,8 +2,19 @@ package com.learning.coursestudent.classes;
 
 public class University {
 
+
     public University() {
-        String universityName = "University of Glasgow";
-        String universitySlogan = "Via, Veritas, Vita";
+    }
+
+    public static String getUniversityName() {
+        return "University of Glasgow";
+    }
+
+    public static String getUniversitySlogan() {
+        return "Via, Veritas, Vita";
+    }
+
+    public static String getUniversityMailDomain() {
+        return "@uni-glasgow.scot";
     }
 }
